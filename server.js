b73d9e1a49087dde296a4a4665c8f5e0bd628728
Ucheduk <dukweb4@gmail.com>
2020-08-19 16:22:57 +0100

    feat: can get and remove joined users

--- server.js
+++ server.js
@@ -9,9 +9,48 @@ const io = socketio(server);
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
 
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require('./users')
+
 io.on('connection', socket => {
-  console.log('user connected!!!');
-  socket.on('disconnect', () => console.log('user disconnect!!!'))
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(`${name} connected to room ${room}`);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the room ${user.room}`
+    });
+
+    socket.broadcast.to(user.room)
+      .emit('message', {
+        user: 'admin',
+        text: `${user.name}, has joined!`
+      })
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', {
+      user: user.name,
+      text: message
+    });
+
+    callback();
+  });
+
+  socket.on('disconnect', name => console.log(`${name} disconnect!`))
 });
 
 app.use(router);
